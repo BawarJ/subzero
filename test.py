69d9f8db687717f7b92a4cfc27bc2e6f16a1b153
Bawar Jalal <bawar96@hotmail.com>
2016-01-23 13:21:05 +0000

    audio working with single note

--- test.py
+++ test.py
@@ -1,16 +1,47 @@
-import pygame.mixer
-from pygame.mixer import Sound
+import pygame, sys
+from pygame.locals import *
+from pygame.mixer import Channel, Sound, get_init, pre_init
+from array import array
+from time import sleep
 
+class Note(Sound):
+
+    def __init__(self, frequency, volume=.01):
+        self.frequency = frequency
+        Sound.__init__(self, buffer=self.build_samples())
+        self.set_volume(volume)
+
+    def build_samples(self):
+        period = int(round(get_init()[0] / self.frequency))
+        samples = array("h", [0] * period)
+        amplitude = 2 ** (abs(get_init()[1]) - 1) - 1
+        for time in xrange(period):
+            if time < period / 2:
+                samples[time] = amplitude
+            else:
+                samples[time] = -amplitude
+        return samples
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400,300))
+pygame.display.set_caption('SubZero')
+
+pygame.mixer.pre_init(44100, -16, 2, 1024)
 pygame.mixer.init()
 
 drum = Sound("samples/dance116.wav")
+pygame.mixer.Sound.set_volume(drum, 0.5)
 
 while True:
+    pygame.mixer.Channel(0).queue(drum)
+
+    pressed = pygame.key.get_pressed()
+    
+    if pressed[pygame.K_w]:
+        pygame.mixer.Channel(1).play(Note(220))
+
     for event in pygame.event.get():
-        if event.type == QUIT:                                                    
+        if event.type == QUIT:
             pygame.quit()
-        if event.type == KEYDOWN:
-            if event.key==K_ESCAPE:
-                 pygame.quit()
-            elif event.key==K_UP:
-            	drum.play()
+            sys.exit()
+    pygame.display.update()
